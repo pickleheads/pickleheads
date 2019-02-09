2a96ea28113a39307f8bfe7f3ea96edd49b858f1
Zack Harley <zackharley@gmail.com>
2019-02-08 19:34:30 -0500

    Add netlify config

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="container footer">
+    <p class="footer-title">Want us to build something for you?</p>
+    <p>We don't do that</p>
+    <a href="mailto:hello@pickleheads.club" class="button">
+      Reach Out
+    </a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped>
+  .footer {
+    background-color: #333;
+    color: white;
+  }
+
+  .footer-title {
+    font-size: 1.4rem;
+    font-family: 'Playfair Display', serif;
+  }
+</style>
